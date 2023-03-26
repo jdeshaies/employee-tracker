973d42d87b3bc0b0bdd7f27061edd7104ba793f1
jd479800 <jd479800@gmail.com>
2023-03-26 15:28:48 -0400

    Adds function to view all employees

--- index.js
+++ index.js
@@ -39,6 +39,9 @@ const askUser = () => {
       if (choice === "View All Roles") {
         viewRoles();
       }
+      if (choice === "View All Employees") {
+        viewEmployees();
+      }
     });
 };
 
@@ -55,7 +58,35 @@ const viewDepartments = () => {
 };
 
 const viewRoles = () => {
-  const query = `SELECT role.id, role.title, department.name AS department, role.salary FROM role INNER JOIN department ON role.department_id = department.id ORDER BY role.id;`;
+  const query = `SELECT role.id, 
+                role.title, 
+                department.name AS department, 
+                role.salary 
+                FROM role 
+                INNER JOIN department ON role.department_id = department.id 
+                ORDER BY role.id;`;
+  db.promise()
+    .query(query)
+    .then(([rows]) => {
+      console.log("\n");
+      console.table(rows);
+    })
+    .catch(console.log)
+    .then(() => db.end());
+};
+
+const viewEmployees = () => {
+  const query = `SELECT employee.id, 
+                employee.first_name, 
+                employee.last_name, 
+                role.title,
+                department.name AS department, 
+                role.salary, 
+                CONCAT(manager.first_name, " ", manager.last_name) AS manager
+                FROM employee
+                LEFT JOIN role ON employee.role_id = role.id
+                LEFT JOIN department ON role.department_id = department.id
+                LEFT JOIN employee manager ON employee.manager_id = manager.id`;
   db.promise()
     .query(query)
     .then(([rows]) => {
